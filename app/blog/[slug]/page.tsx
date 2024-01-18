570bf5f4c48e1be535984cdc1087a45ed53ea409
syun_m <sm.net_minecraft.hacker@outlook.jp>
2024-01-18 11:06:58 +0900

    format

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,20 +1,20 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import { unified } from 'unified';
-import remarkParse from 'remark-parse';
-import remarkHtml from 'remark-html';
-import Link from 'next/link';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkHtml from "remark-html";
+import Link from "next/link";
 
 // ブログ記事一覧を取得
-const postsDirectory = path.join(process.cwd(), 'contents');
+const postsDirectory = path.join(process.cwd(), "contents");
 const fileNames = fs.readdirSync(postsDirectory);
 const posts = fileNames.map((fileName) => {
   const filePath = path.join(postsDirectory, fileName);
-  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const fileContents = fs.readFileSync(filePath, "utf8");
   const { data } = matter(fileContents);
   return {
-    slug: fileName.replace(/\.md$/, ''),
+    slug: fileName.replace(/\.md$/, ""),
     frontmatter: data,
   };
 });
@@ -23,20 +23,25 @@ const posts = fileNames.map((fileName) => {
 export default async function BlogPost({ params }: { params: any }) {
   // URLのパラメータから該当するファイル名を取得 (今回は hello-world)
   const { slug } = params;
-  const filePath = path.join(process.cwd(), 'contents', `${slug}.md`);
+  const filePath = path.join(process.cwd(), "contents", `${slug}.md`);
 
   // ファイルの中身を取得
-  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const fileContents = fs.readFileSync(filePath, "utf8");
   const { data, content } = matter(fileContents);
   const title = data.title; // 記事のタイトル
-  const processedContent = await unified().use(remarkParse).use(remarkHtml).process(content);
+  const processedContent = await unified()
+    .use(remarkParse)
+    .use(remarkHtml)
+    .process(content);
   const contentHtml = processedContent.toString(); // 記事の本文をHTMLに変換
 
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Blog</h2>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Blog
+          </h2>
           <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
             {posts.map((post) => (
               <article
