2737a0cf4cc9b29728946d1174c380031b0bb462
syun_m <sm.net_minecraft.hacker@outlook.jp>
2024-01-18 11:32:07 +0900

    contents内のmdを取得して表示

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import './content.css';
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
@@ -23,6 +24,7 @@ const posts = fileNames.map((fileName) => {
 export default async function BlogPost({ params }: { params: any }) {
   // URLのパラメータから該当するファイル名を取得 (今回は hello-world)
   const { slug } = params;
+  // ファイルのパスを取得
   const filePath = path.join(process.cwd(), "contents", `${slug}.md`);
 
   // ファイルの中身を取得
@@ -36,32 +38,15 @@ export default async function BlogPost({ params }: { params: any }) {
   const contentHtml = processedContent.toString(); // 記事の本文をHTMLに変換
 
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Blog
-          </h2>
-          <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
-            {posts.map((post) => (
-              <article
-                key={post.slug}
-                className="flex max-w-xl flex-col items-start justify-between"
-              >
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <Link
-                      href={`/blog/${post.slug}`}
-                      className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                  </h3>
-                </div>
-              </article>
-            ))}
-          </div>
-        </div>
+    <div className="bg-white px-6 py-32 lg:px-8">
+      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          {title}
+        </h1>
+        <div
+          className="mt-6"
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+        ></div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import Link from 'next/link';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Link from "next/link";
 
 export default async function Blogs() {
   // contentディレクトリ内のマークダウンファイル一覧を取得
-  const postsDirectory = path.join(process.cwd(), 'contents');
+  const postsDirectory = path.join(process.cwd(), "contents");
   const fileNames = fs.readdirSync(postsDirectory);
 
   // 各ファイルの中身を取得
@@ -13,12 +13,12 @@ export default async function Blogs() {
     // 各ファイル情報を取得
     fileNames.map(async (fileName) => {
       const filePath = path.join(postsDirectory, fileName);
-      const fileContents = fs.readFileSync(filePath, 'utf8');
+      const fileContents = fs.readFileSync(filePath, "utf8");
       const { data } = matter(fileContents);
 
       // slugとfrontmatter(title, date, description)を取得
       return {
-        slug: fileName.replace('.md', ''),
+        slug: fileName.replace(".md", ""),
         frontmatter: data,
       };
     })
@@ -28,15 +28,49 @@ export default async function Blogs() {
   );
 
   return (
-    <div>
-      <h1>My Blog</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/blog/${post.slug}`}>{post.frontmatter.title}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Blog
+          </h2>
+          <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
+            {posts.map((post) => (
+              <article
+                key={post.slug}
+                className="flex max-w-xl flex-col items-start justify-between"
+              >
+                <div className="group relative">
+                  {/* 日付を表示 */}
+                  <div className="text-sm text-gray-500">
+                    <div className="flex items-center">
+                      <time dateTime={post.frontmatter.date}>
+                        {post.frontmatter.date}
+                      </time>
+                    </div>
+                  </div>
+                  {/* 記事タイトル・リンク */}
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-blue-700 group-hover:text-blue-400">
+                    <Link
+                      href={`/blog/${post.slug}`}
+                      className="mt-3 text-lg font-semibold leading-6 text-blue-700 group-hover:text-blue-400"
+                    >
+                      {post.frontmatter.title}
+                    </Link>
+                  </h3>
+                  {/* 記事説明文を表示 */}
+                  <p
+                    className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"
+                    dangerouslySetInnerHTML={{
+                      __html: `${post.frontmatter.description}`,
+                    }}
+                  ></p>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
