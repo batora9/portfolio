34942e6cc126ec9aab8f3eecf46f2f699effa605
syun_m <sm.net_minecraft.hacker@outlook.jp>
2024-02-25 13:06:09 +0900

    メインページの編集

--- app/page.tsx
+++ app/page.tsx
@@ -2,36 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-black h-screen flex flex-col justify-center">
-      <h1 className="text-4xl text-center text-white">Welcome to my page!</h1>
-      <p className="text-center text-white">This is my first Next.js page.</p>
-      <a href="/blogs" className="text-center text-white">
-        Blogs
-      </a>
-      <div className="flex">
-        <img src="/favicon.ico" alt="favicon" className="size-32" />
-        <h1 className="text-4xl text-center text-white">Batora9</h1>
-        <Link
-          href="https://github.com/batora9"
-          target="_blank"
-          rel="noopener noreferrer"
-          passHref
-        >
-          <img
-            src="/github-mark-white.svg"
-            alt="github"
-            className="mx-auto size-8"
-          />
-        </Link>
-        <Link
-          href="https://x.com/265"
-          target="_blank"
-          rel="noopener noreferrer"
-          passHref
-        >
-          <img src="/logo-white.png" alt="x" className="mx-auto size-8" />
-        </Link>
-      </div>
+    <div className="bg-black h-screen flex items-center justify-center flex-col">
+      <img src="/favicon.ico" alt="logo" className="h-32 w-32" />
+      <h1 className="text-white text-4xl font-bold">BATORA</h1>
+      <p className="text-white">SOFTWARE / WEB ENGINEER</p>
     </div>
   );
 }
