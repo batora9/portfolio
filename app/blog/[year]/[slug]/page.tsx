578a1705e69b1c197ee5aabd6c042ba279804258
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-03-03 03:54:51 +0900

    記事のスタイリング

--- app/blog/[year]/[slug]/page.tsx
+++ app/blog/[year]/[slug]/page.tsx
@@ -6,6 +6,8 @@ import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkHtml from "remark-html";
 import { Metadata } from "next";
+import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
 
 export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
@@ -43,6 +45,9 @@ export default async function BlogPost( { params } : Props ) {
   const fileContents = fs.readFileSync(filePath, "utf8");
   const { data, content } = matter(fileContents);
   const title = data.title; // 記事のタイトル
+  const description = data.description; // 記事のディスクリプション
+  const date = data.date; // 記事の日付
+
   metadata.title = title; // ページのタイトルを記事のタイトルにする
   metadata.description = data.description; // ページのディスクリプションを記事のディスクリプションにする
 
@@ -53,16 +58,21 @@ export default async function BlogPost( { params } : Props ) {
   const contentHtml = processedContent.toString(); // 記事の本文をHTMLに変換
 
   return (
-    <div className="bg-white px-6 py-32 lg:px-8">
-      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          {title}
-        </h1>
+    <div className="flex bg-black flex-col min-h-screen">
+      <Header />
+      <div className="bg-black px-6 py-12 lg:px-8">
+        <div className="mx-auto max-w-3xl text-base leading-7 text-white">
+          <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+            {title}
+          </p>
+          <p className="mt-2 text-sm text-gray-400">作成日 {date}</p>
+        </div>
         <div
-          className="mt-6"
+          className="prose prose-lg text-white mx-auto max-w-3xl mt-8"
           dangerouslySetInnerHTML={{ __html: contentHtml }}
-        ></div>
+        />
       </div>
+      <Footer />
     </div>
   );
 }
